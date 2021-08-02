60ce864680496ed80ae60599e6918f518cd73984
Kenneth Heafield <github@kheafield.com>
2021-08-02 09:17:45 +0000

    Find correct path.  Rename source file to .src.

--- gender-test-data/apply_generate.sh
+++ gender-test-data/apply_generate.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
-set -e
-python3 ./generate.py terms.txt fofc.txt > fofc.en.full
+set -eo pipefail
+base="$(dirname "$0")"
+python3 "$base"/generate.py "$base"/terms.txt "$base"/fofc.txt > "$base"/fofc.en.full
 
-python3 ./generate.py terms.txt momc.txt > momc.en.full
+python3 "$base"/generate.py "$base"/terms.txt "$base"/momc.txt > "$base"/momc.en.full
 
-python3 ./generate.py terms.txt mofc.txt >  mofc.en.full
+python3 "$base"/generate.py "$base"/terms.txt "$base"/mofc.txt > "$base"/mofc.en.full
 
-python3 ./generate.py terms.txt fomc.txt  > fomc.en.full
+python3 "$base"/generate.py "$base"/terms.txt "$base"/fomc.txt > "$base"/fomc.en.full
 
-python3 ./generate.py terms.txt anti-stereotype.txt  > anti-stereotype.en.full
-python3 ./generate.py terms.txt stereotype.txt  > stereotype.en.full
+python3 "$base"/generate.py "$base"/terms.txt "$base"/anti-stereotype.txt  > "$base"/anti-stereotype.en.full
+python3 "$base"/generate.py "$base"/terms.txt "$base"/stereotype.txt  > "$base"/stereotype.en.full
 
 for ff in fofc momc fomc mofc stereotype anti-stereotype; do 
-  cut -f1 $ff.en.full |sed 's/ \.$/./;s/ !$/!/;s/ , /, /g; s/ &apos;s/'\''s/g' > $ff.en.full.new
-  cut -f2- $ff.en.full > $ff.en.full.tags
+  cut -f1 "$base"/$ff.en.full |sed 's/ \.$/./;s/ !$/!/;s/ , /, /g; s/ &apos;s/'\''s/g' > "$base/"$ff.en.src
+  cut -f2- "$base"/$ff.en.full > "$base/"$ff.en.tags
 done
