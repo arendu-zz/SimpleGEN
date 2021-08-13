cc256cacdb5aa9900d612366bf2bf17c82180c75
Adi Renduchintala <adirendu@devfair054.h1.fair>
2021-08-13 15:55:37 -0700

    split generate and evaluate

--- make_sources.sh
+++ make_sources.sh
@@ -20,11 +20,3 @@ for ff in fofc momc fomc mofc stereotype anti-stereotype; do
   \rm "$base"/$ff.en.full 
 done
 
-#evaluate a translation
-#example translation file from our paper
-for s in 101 202 303; do 
-  python3 eval.py "$base/"sources/mofc.en.v0.src \
-    "$base/"sources/mofc.en.expected_gender \
-    "$base/"outputs/mofc.seed${s}.bs1.archtransformer_vaswani_wmt_en_de_big.ngpu8.bs1.en-es.out \
-    "$base"/dictionaries/dictionary-en-es-new.csv > "$base"/outputs/mofc.seed${s}.bs1.archtransformer_vaswani_wmt_en_de_big.ngpu8.bs1.en-es.result
-done
